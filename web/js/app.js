d979b1a2fec0de2333e487bbf8f7e161f06f2704
kushkamisha <kushka.misha@gmail.com>
2019-09-21 20:03:20 +0300

    Call smart contract's function

--- web/js/app.js
+++ web/js/app.js
@@ -1,8 +1,24 @@
+function callContract() {
+    const jsonInterface = [{ "constant": true, "inputs": [], "name": "getName", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_addr", "type": "address" }], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }]
+    const address = '0x13A31b34F5999D6677C41559E7e8e2CeF9ce7674' // Caller
+    const contract = new web3.eth.Countract(jsonInterface, address)
+
+    console.log({ contract })
+    contract.methods.getName().call((err, name) => {
+        console.log({ name })
+    })
+}
+
 $(document).ready(function() {
-  const web3 = new Web3(window.web3.currentProvider);
 
-  const address = "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07"
-
-  const contract = window.web3.eth.contract(abi, address)
+    ethereum.enable()
+    .then(function (accounts) {
+        console.log({ accounts })
+        
+        callContract()
+    })
+    .catch(function (error) {
+        console.error(error)
+    })
 });
 
