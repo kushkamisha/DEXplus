dbf09b19b76f07e995f74cc727ddd52dbfa75283
DenyStark <denyskrolevetskyi@gmail.com>
2019-09-21 16:15:14 +0300

    Feat: Create order logic

--- Contracts/Exchange.sol
+++ Contracts/Exchange.sol
@@ -10,9 +10,11 @@ import "./ERC20.sol";
 interface ExchangeInterface {
     function setERC20Token(uint index, address token) external;
     function setMainStatus(bool status) external;
+    function createOrederERC20(uint price, uint amount, uint tokenId, uint expireDate) external returns(uint index);
 
     event SetERC20Token(uint index, address token);
     event SetMainStatus(bool mainStatus);
+    event CreateOrederERC20(uint price, uint amount, uint tokenId, uint expireDate);
 }
 
 
@@ -24,15 +26,56 @@ interface ExchangeInterface {
 contract Exchange is ExchangeInterface, Roles {
     bool public mainStatus;
 
+    struct OrderERC20 {
+        uint price;
+        uint amount;
+        uint tokenId;
+        uint expireDate;
+    }
+
     mapping (uint => ERC20Interface) public ERC20tokens;
+
+    mapping (uint => OrderERC20) public ordersERC20;
+    uint ordersCountERC20;
 
     constructor() public {
         mainStatus = true;
+        ordersCountERC20 = 0;
     }
 
     modifier isActive {
         require(mainStatus, "Platform is stopped.");
         _;
+    }
+
+    modifier checkOrder(uint amount, uint tokenId, uint expireDate) {
+        require(ERC20tokens[tokenId].allowance(msg.sender, address(this)) >= amount, "Not enought allowance.");
+        require(expireDate > now, "Wrong expire date.");
+        _;
+    }
+
+    /**
+     * @dev Create ERC20 oreder.
+     * @param price uint The ETH price
+     * @param amount uint The token amount
+     * @param tokenId uint The token id
+     * @param expireDate uint The expire date in timestamp
+     */
+    function createOrederERC20(uint price, uint amount, uint tokenId, uint expireDate) isActive checkOrder(amount, tokenId, expireDate) external returns(uint index) {
+        ERC20tokens[tokenId].transferFrom(msg.sender, address(this), amount);
+        
+        OrderERC20 memory order;
+        index = ordersCountERC20;
+
+        order.price      = price;
+        order.amount     = amount;
+        order.tokenId    = tokenId;
+        order.expireDate = expireDate;
+
+        ordersERC20[index] = order;
+        ordersCountERC20++;
+        
+        emit CreateOrederERC20(price, amount, tokenId, expireDate);
     }
 
     /**
